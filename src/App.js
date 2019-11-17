9fb5b6848b558ac46ee218a0999e801fe17bbbc6
travism26 <travismartin266@gmail.com>
2019-11-16 22:58:15 -0400

    update nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import './css/styles.css'
 
 function App() {
   return (
     <div className="App">
-      Travis Martin
+      <nav className="nav-menu">
+        <h2 className="logo"><a className="logo-link" href="#">Things to look at</a></h2>
+        <ul>
+          <li><a className="nav-menu__link" href="#">Python Scraper</a></li>
+          <li><a className="nav-menu__link" href="#">NLP</a></li>
+          <li><a className="nav-menu__link" href="#">Traveling</a></li>
+        </ul>
+      </nav>
     </div>
   );
 }
