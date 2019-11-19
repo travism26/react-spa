5f3b49d89d6c98e7176bc9be1e251cff9598dec4
travism26 <travismartin266@gmail.com>
2019-11-18 21:44:19 -0400

    add few more things

--- src/components/traveling.js
+++ src/components/traveling.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { TravelingList } from '../data/traveling';
+import { Link } from 'react-router-dom';
 
-const Traveling = (props) => (
+const Traveling = (props) => {
+
+    let places_to_travel = TravelingList.map((travel) => {
+        return (
+            <div className="travel-container">
+                <a>
+                    {/* Feel like this is a security risk doing this... */}
+                    <div className="travel-image" style={{ backgroundImage: "url(" + travel.capital_img + ")" }}></div>
+                </a>
+                <h3>{travel.country}</h3>
+            </div>
+        )
+    });
+    return (
     <div className="main-content">
+        <div><Link className="back" to="/">Back</Link></div>
+        <h2>{props.title}</h2>
         <div className="container">
-            <h2>This is the traveling page</h2>
+            {places_to_travel}
         </div>
     </div>
-);
+    )
+};
 
 export default Traveling
---
+++ src/data/traveling.js
@@ -0,0 +1,32 @@
+export const TravelingList = [
+    {
+        country: "Italy",
+        capital: "Rome",
+        travel_interest: "Food and ancient city",
+        capital_img: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Rome_Montage_2017.png/856px-Rome_Montage_2017.png"
+    },
+    {
+        country: "England",
+        capital: "London",
+        travel_interest: "Beautiful City and archtecture",
+        capital_img: "https://upload.wikimedia.org/wikipedia/commons/a/a4/London_Ontario_Montage.png"
+    },
+    {
+        country: "France",
+        capital: "Paris",
+        travel_interest: "City of love",
+        capital_img: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/08/07/08/paris.jpg?w968"
+    },
+    {
+        country: "Germany",
+        capital: "Arnold Schwarzenegger",
+        travel_interest: "Wasnt Arnold born in or around this country?",
+        capital_img: "https://www.worldatlas.com/r/w728-h425-c728x425/upload/07/f3/e1/shutterstock-309376361.jpg"
+    },
+    {
+        country: "USA",
+        capital: "Washington",
+        travel_interest: "Murica",
+        capital_img: "https://upload.wikimedia.org/wikipedia/commons/8/81/Washington_Montage_2016.png"
+    }
+];
